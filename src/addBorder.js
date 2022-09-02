490dc224d110516b4f0b30a258c4f8be6bc976c0
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 14:43:12 -0300

    update addBorder.js

--- src/addBorder.js
+++ src/addBorder.js
@@ -15,3 +15,18 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test addBorder
+
+// alternative solution
+// function solution(picture) {
+//     let width = picture[0].length + 2;
+//     return ['*'.repeat(width), ...picture.map((line) => `*${line}*`), '*'.repeat(width)];
+// }
+
+// alternative solution
+// solution = (p) => [(t = p[0].replace(/./g, '*') + '**'), ...p.map((v) => `*${v}*`), t];
+
+// alternative solution
+// function solution(picture) {
+//     let r = '*'.repeat(picture[0].length + 2);
+//     return [r, ...picture.map((x) => `*${x}*`), r];
+// }
